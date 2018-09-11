d6f8bb789c3d932b9f06f3834952b9eb53833e71
urosh <urosh.stojkovikj@gmail.com>
2018-09-11 09:34:11 -0700

    complete graph & install order

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,15 +1,32 @@
 class Vertex
-  def initialize(value)
+  attr_reader :in_edges, :out_edges, :value
 
+  def initialize(value)
+    @value = value
+    @in_edges = []
+    @out_edges = []
   end
 end
 
 class Edge
-  def initialize(from_vertex, to_vertex, cost = 1)
+  attr_reader :from_vertex, :to_vertex, :cost
 
+  def initialize(from_vertex, to_vertex, cost = 1)
+    @from_vertex = from_vertex
+    @to_vertex = to_vertex
+    @cost = cost
+    @to_vertex.in_edges.push(self)
+    @from_vertex.out_edges.push(self)
   end
 
   def destroy!
-
+    to_vertex.in_edges.delete(self)
+    from_vertex.out_edges.delete(self)
+    @to_vertex = nil
+    @from_vertex = nil
   end
+
+  private
+  
+  attr_writer :from_vertex, :to_vertex, :cost
 end
--- lib/install_order.rb
+++ lib/install_order.rb
@@ -10,5 +10,34 @@
 
 
 def install_order(arr)
+  list_el = []
+  graph = []
+  top = []
+  arr.each do |el|
+    list_el.push(el[0]) unless list_el.include?(el[0])
+    graph.push(Edge.new(Vertex.new(el[1]), Vertex.new(el[0])))
+  end
 
+  sorted = []
+  (1..list_el.max).each do |idx|
+    sorted.push(idx) unless list_el.include?(idx)
+  end
+
+  graph.each do |task|
+    if task.from_vertex.in_edges.empty?
+      top.push(task.from_vertex)
+    end
+  end
+
+  until top.empty?
+    current = top.shift
+    sorted << current.value unless sorted.include?(current.value)
+    current.out_edges.each do |edge|
+      edge.to_vertex.in_edges.delete(edge)
+      if edge.to_vertex.in_edges.empty?
+        top.push(edge.to_vertex)
+      end
+    end
+  end
+  sorted
 end
